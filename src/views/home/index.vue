6f4ab425dfcba0cb529078363545633086c11793
fzp <350556636@qq.com>
2019-10-22 10:24:04 +0800

    完成home的信息列表框架

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -13,7 +13,7 @@
           <!-- load：1）当页面加载时会默认调用一次，2）当上拉触底时，会触发这个方法 -->
           <!-- 这个方法有个坑：会自动将 list 中的 v-model 中对应属性设置 true,并且不自动设置为 false,在使用时，需要手动设置为 false -->
           <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
-            <van-cell v-for="item in 14" :key="item" :title="item" />
+            <van-cell v-for="item in list" :key="item" :title="item" />
           </van-list>
         </van-tab>
       </van-tabs>
@@ -38,7 +38,20 @@ export default {
     }
   },
   methods: {
-    onload () {}
+    onLoad () {
+      // 扩展运算符(三点运算符)
+      // 前面的数据源(this.list) 加上后面加载的10条数据(更新的数据)
+      // 让list 在原有数据的基础上再添加 1-10个数据
+      this.list = [...this.list, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+      // 判断内容触底, 不能再下拉
+      if (this.list.length >= 80) {
+        // 已经加载完成
+        this.finished = true
+      }
+      console.log('3211')
+      // 重新设置v-model中的loading的值
+      this.loading = false
+    }
   }
 }
 </script>
